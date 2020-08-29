f6ad143b72183f91aacdd5058f250bbd54c1acb3
renato_O <renatorsdeoliveira@gmail>
2020-08-29 08:14:22 -0300

    Terminado o segundo modulo

---
+++ wp-content/themes/lojavirtual/footer.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Fancy Lab
+ */
+?>
+		<footer>
+			<section class="footer-widgets">
+				<div class="container">
+					<div class="row">Widgets do rodapé</div>
+				</div>
+			</section>
+			<section class="copyright">
+				<div class="container">
+					<div class="row">
+						<div class="copyright-text col-12 col-md-6">Copyright</div>
+						<div class="footer-menu col-12 col-md-6 text-left text-md-right">
+							<?php 
+								wp_nav_menu(
+									array(
+										'theme_location'	=> 'fancy_lab_footer_menu'
+									)
+								);
+							 ?>
+						</div>
+					</div>
+				</div>
+			</section>
+		</footer>
+	</div>
+<?php wp_footer(); ?>
+</body>
+</html>
---
+++ wp-content/themes/lojavirtual/functions.php
@@ -0,0 +1,30 @@
+<?php 
+
+/**
+ * Fancy Lab functions and definitions
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package Fancy Lab
+ */
+
+/**
+* Enqueue scripts and styles.
+*/
+function fancy_lab_scripts(){
+	wp_enqueue_script( 'bootstrap-js', get_template_directory_uri() . '/inc/bootstrap.min.js', array( 'jquery' ), '4.4.1', true );
+	wp_enqueue_style( 'bootstrap-css', get_template_directory_uri() . '/inc/bootstrap.min.css', array(), '4.4.1', 'all' );
+ 	// Theme's main stylesheet
+ 	wp_enqueue_style( 'fancy-lab-style', get_stylesheet_uri(), array(), filemtime( get_template_directory() . '/style.css' ), 'all' );
+}
+add_action( 'wp_enqueue_scripts', 'fancy_lab_scripts' );
+
+function fancy_lab_config(){
+	register_nav_menus(
+		array(
+			'fancy_lab_main_menu'	=> 'Fancy Lab Main Menu',
+			'fancy_lab_footer_menu'	=> 'Fancy Lab Footer Menu'
+		)
+	);
+}
+add_action( 'after_setup_theme', 'fancy_lab_config', 0 );
